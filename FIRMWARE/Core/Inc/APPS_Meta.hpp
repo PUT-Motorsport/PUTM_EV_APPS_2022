26640db1b7820821282ea0d6a21d1220fd6402fd
Patryk Gawron <patryk.gawron@outlook.com>
2022-05-31 22:22:41 +0200

    Calbirated APPS for table testing

--- FIRMWARE/Core/Inc/APPS_Meta.hpp
+++ FIRMWARE/Core/Inc/APPS_Meta.hpp
@@ -34,15 +34,15 @@ pedal travel and 0 N m desired motor torque, regardless of whether the brakes ar
 const float apps_dead_zone = 0.05;
 
 // const for apps sensor 1
-const int APPS_1_RAW_MIN = 2826;
-const int APPS_1_RAW_MAX = 3350;
+const int APPS_1_RAW_MIN = 2966;
+const int APPS_1_RAW_MAX = 3555;
 static_assert(APPS_1_RAW_MIN < APPS_1_RAW_MAX);
 const int APPS_1_RAW_FULLSCALE = APPS_1_RAW_MAX - APPS_1_RAW_MIN;
 const int APPS_1_OFFSETTED_MIN = (int)std::round( APPS_1_RAW_FULLSCALE * apps_dead_zone + APPS_1_RAW_MIN );
 
 // const for apps sensor 2
-const int APPS_2_RAW_MIN = 2854;
-const int APPS_2_RAW_MAX = 3380;
+const int APPS_2_RAW_MIN = 3300;
+const int APPS_2_RAW_MAX = 3720;
 static_assert(APPS_2_RAW_MIN < APPS_2_RAW_MAX);
 const int APPS_2_RAW_FULLSCALE = APPS_2_RAW_MAX - APPS_2_RAW_MIN;
 const int APPS_2_OFFSETTED_MIN = (int)std::round( APPS_2_RAW_FULLSCALE * apps_dead_zone + APPS_2_RAW_MIN);
